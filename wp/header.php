c00a690364145f1fc8120c893fc518e538365f13
alizadeh88 <alizadeh8822443018@gmail.com>
2012-10-22 17:34:38 +0330

    edit header.php

---
+++ wp/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="fa-IR" dir="rtl">
+<head>
+	<meta charset="utf-8" />
+	<title><?php bloginfo('name'); wp_title(); ?></title>
+	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico" type="image/x-icon">
+	<script src="<?php bloginfo('template_url'); ?>/common-file/zepto.js" type="text/javascript"></script>
+	<link href="<?php bloginfo('template_url'); ?>/common-file/reset.css" type="text/css" rel="stylesheet" />
+	<link href="<?php bloginfo('template_url'); ?>/common-file/style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
