bdb814fe8fa5311cff39dc31aef3e63d5a7a0212
alizadeh88 <alizadeh8822443018@gmail.com>
2012-10-22 15:57:58 +0330

    add nav.php

---
+++ wp/nav.php
@@ -0,0 +1,42 @@
+<div class="container">
+	<div class="container-header">
+		<div class="header"></div>
+	</div>
+	<div class="content">
+		<div class="top-content">
+			<div class="menu-index">
+				<div class="container-menu-index">
+					<ul>
+						<li>
+							<a href="index.html">
+								<img class="ico" src="images/menu-home.png" width="50" height="50" />
+								<span class="lbl">صفحه اصلی</span>
+							</a>
+						</li>
+						<li>
+							<a href="newDrug.html">
+								<img class="ico" src="images/menu-newDrug.png" width="50" height="50" />
+								<span class="lbl">تــازه های دارویی</span>
+							</a>
+						</li>
+						<li>
+							<a href="company.html">
+								<img class="ico" src="images/menu-company.png" width="50" height="50" />
+								<span class="lbl">شرکت های داروسازی</span>
+							</a>
+						</li>
+						<li>
+							<a href="diary.html">
+								<img class="ico" src="images/menu-memory.png" width="50" height="50" />
+								<span class="lbl">خاطرات داروخانه</span>
+							</a>
+						</li>
+						<li>
+							<a href="aboutUs.html">
+								<img class="ico" src="images/menu-aboutUs.png" width="50" height="50" />
+								<span class="lbl">دربــــاره ما</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
